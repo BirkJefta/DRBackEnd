8a490a52495972aed27b15bf73521fef8757a2fe
Birk Jefta Svensson <bis001@edu.zealand.dk>
2025-05-02 12:50:43 +0200

    igen

--- DRBackEnd/Models/DRPladeModel.cs
+++ DRBackEnd/Models/DRPladeModel.cs
@@ -7,7 +7,7 @@ public class DRPladeModel
     public string Artist { get; set; }
 
     public int Duration { get; set; }
-
+     
     public int PublicationYear { get; set; }
 
     public DRPladeModel(string title, string artist, int duration, int publicationYear)
